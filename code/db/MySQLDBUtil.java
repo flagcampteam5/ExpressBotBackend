4c6dc13d08d4a1417cdbfdf9b3939e2d1a1b8776
flagcampteam5 <69276519+flagcampteam5@users.noreply.github.com>
2020-08-30 17:59:23 -0700

    Update MySQLDBUtil.java

--- code/db/MySQLDBUtil.java
+++ code/db/MySQLDBUtil.java
@@ -5,7 +5,7 @@ public class MySQLDBUtil {
     private static final String PORT_NUM = "3306";
     public static final String DB_NAME = "xxx";
     private static final String USERNAME = "admin";
-    private static final String PASSWORD = "xxx";
+    private static final String PASSWORD = "xxxx";
     public static final String URL = "jdbc:mysql://"
             + INSTANCE + ":" + PORT_NUM + "/" + DB_NAME
             + "?user=" + USERNAME + "&password=" + PASSWORD
