44e3d3462153540830e4b29fb9ba500a1f73ea8f
TheSubtleArt <frankchen@berkeleyl.edu>
2020-08-30 17:52:44 -0700

    db week5 first commit

--- code/db/MySQLDBUtil.java
+++ code/db/MySQLDBUtil.java
@@ -1,11 +1,11 @@
 package db;
 
 public class MySQLDBUtil {
-    private static final String INSTANCE = "expressbot-instance.clbji0cftfqo.us-east-2.rds.amazonaws.com";
+    private static final String INSTANCE = "xxx";
     private static final String PORT_NUM = "3306";
-    public static final String DB_NAME = "expressBot";
+    public static final String DB_NAME = "xxx";
     private static final String USERNAME = "admin";
-    private static final String PASSWORD = "livingreckless";
+    private static final String PASSWORD = "xxx";
     public static final String URL = "jdbc:mysql://"
             + INSTANCE + ":" + PORT_NUM + "/" + DB_NAME
             + "?user=" + USERNAME + "&password=" + PASSWORD
