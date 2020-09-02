5f34715aadb21e144b3dc1c2f1e9935841f8593d
flagcampteam5 <69276519+flagcampteam5@users.noreply.github.com>
2020-09-01 18:22:32 -0700

    Update MySQLTableCreation.java

--- code/db/MySQLTableCreation.java
+++ code/db/MySQLTableCreation.java
@@ -3,13 +3,13 @@ package db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
-import java.sql.Timestamp;
+
 
 public class MySQLTableCreation {
 	// Run this as Java application to reset the database.
 	public static void main(String[] args) {
 		try {
-			// Step 1 Connect to MySQL.
+			// Step 1: Connect to MySQL.
 			System.out.println("Connecting to " + MySQLDBUtil.URL);
 			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
 			Connection conn = DriverManager.getConnection(MySQLDBUtil.URL);
@@ -17,16 +17,10 @@ public class MySQLTableCreation {
 			if (conn == null) {
 				return;
 			}
-			// Step 2 Drop tables in case they exist.
+			// Step 2: Drop tables in case they exist.
 			Statement statement = conn.createStatement();
-
-			String sql = "DROP TABLE IF EXISTS users";
-			statement.executeUpdate(sql);
-
+			
 			sql = "DROP TABLE IF EXISTS transactions";
-			statement.executeUpdate(sql);
-
-			sql = "DROP TABLE IF EXISTS status";
 			statement.executeUpdate(sql);
 
 			sql = "CREATE TABLE transactions (" + "trans_id VARCHAR(255) NOT NULL," 
@@ -35,26 +29,23 @@ public class MySQLTableCreation {
 					+ "status_id VARCHAR(255) NOT NULL,"					
 					+ "pick_up_lat DOUBLE NOT NULL," + "pick_up_lon DOUBLE NOT NULL,"			
 					+ "deliver_location_lat DOUBLE NOT NULL," + "deliver_location_lon DOUBLE NOT NULL," 
-					+ "timestamp Timestamp NOT NULL," + "PRIMARY KEY (trans_id)"											
+					+ "timestamp INTEGER NOT NULL," + "PRIMARY KEY (trans_id)"											
 					+ ")";
 			statement.executeUpdate(sql);
 			
-	        // Step 4: insert fake user 3229c1097c00d497a0fd282d586be050
-			
-            sql = "INSERT INTO transactions VALUES('3229c1097c00d497a0fd282d586be050', "
+	    		// Step 4: insert fake user 3229c1097c00d497a0fd282d586be050
+            		sql = "INSERT INTO transactions VALUES('3229c1097c00d497a0fd282d586be050', "
             		+ "'1' , '3',"
             		+ " true, false,"
             		+ "'19', "
             		+ "'-120', '30',"
             		+ " '-110', '10', "
-            		+ "'Timestamp.valueOf(\"2020-08-30 00:00:00\")'"
+            		+ "'123456789,'"
             		+ ")";
             	
-            statement.executeUpdate(sql);
-        
+			statement.executeUpdate(sql);
 			conn.close();
 			System.out.println("Import done successfully");
-
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
